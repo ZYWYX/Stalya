60e73af20fc4e836167414ec53e59aa1f02bccd8
Kirua <78649123+kiruaxx@users.noreply.github.com>
2021-02-06 15:50:02 +0100

    Add files via upload

---
+++ resources/fivem/__resource.lua
@@ -0,0 +1,18 @@
+dependency 'basic-gamemode'
+
+files {
+    'index.html',
+    'ui.html',
+    'style.css',
+    'bg/*.png'
+}
+
+loadscreen 'index.html'
+
+ui_page 'ui.html'
+
+client_scripts {
+	'client.lua'
+}
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
